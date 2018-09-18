8cafe9e7f0c953eef9b6ac5131f118f18ef5e267
Dave Taylor <davehtaylor@me.com>
2018-09-18 17:55:29 -0400

    Adding a python file

---
+++ api/py/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+
+# Function for encryption and validation
+# password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+# bcrypt.check_password_hash(password_hash, user_input)
+
+
+# Create the app
+app = Flask(__name__)
+
+# Create the API
+api = Api(app)
+
+# Setup our Bcrypt encryption
+bcrypt = Bcrypt(app)
+
+# Setup database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = "localhost"
+db = SQLAlchemy(app)
+
+
+
+
+
+
