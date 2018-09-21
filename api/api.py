307de9264c4beb5d8a9c5555b2de4e6df9636b65
Dave Taylor <davehtaylor@me.com>
2018-09-21 17:34:38 -0400

    Added a few more functions

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_restful import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
