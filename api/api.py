ff477276505a678032963105d3613b56c8042662
Dave Taylor <davehtaylor@me.com>
2019-01-17 15:03:34 -0500

    Testing out logins and sessions

--- api/api.py
+++ api/api.py
@@ -679,7 +679,7 @@ def dsdMachineByID(machineID):
 def index():
     user = None
     if 'username' in session:
-        user = users.query.filter(users.username == session["username"])
+        user = users.query.filter(users.username == session["username"]).first()
         print("USER: " + user.firstName + " " + user.lastName)
     
     return render_template("index.html", user = user[0])
