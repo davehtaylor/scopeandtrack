71a4d68af8527c623bbc75934e82e805b510d84a
Dave Taylor <davehtaylor@me.com>
2019-01-19 11:02:11 -0500

    Trying out getting data from API with JS

---
+++ api/static/js/orgs.js
@@ -0,0 +1,29 @@
+let baseURL = "http://www.scopeandtrack.com/api"
+
+function getAllOrgs() {
+    let req = new XMLHttpRequest();
+    url = baseURL + "/api/organizations"
+
+    req.open("GET", url, true);
+
+    req.onload = function () {
+
+        // Get the JSON data
+        let data = JSON.parse(this.response);
+
+        if (req.status == 200) {
+            data.forEach( organization => {
+                console.log(organization.name)
+            });
+        }
+        else {
+            console.log("Could not get organizations")
+        }
+    }
+
+    req.send();
+}
+
+function getOrgByID(orgID) {
+
+}
